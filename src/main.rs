838e5e5cadfbeee2038b2f3a5e2c0c7de0253b48
Yanchen Ma <yanchenm@gmail.com>
2021-11-20 04:55:42 -0500

    render first ppm

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use std::{fs::File, io::BufWriter};
+use std::io::Write;
+
 fn main() {
-    println!("Hello, world!");
+    // Image dimensions
+    const IMAGE_WIDTH: u32 = 256;
+    const IMAGE_HEIGHT: u32 = 256;
+
+    // File I/O
+    let file = File::create("output/image.ppm").expect("Unable to create file.");
+    let mut file_buffer = BufWriter::new(file);
+
+    // Write file header
+    let image_header = format!("P3\n{} {}\n255\n", IMAGE_WIDTH, IMAGE_HEIGHT);
+    file_buffer.write(image_header.as_bytes()).unwrap();
+
+    for j in (0..IMAGE_HEIGHT).rev() {
+        for i in 0..IMAGE_WIDTH {
+            let r = i as f64 / (IMAGE_WIDTH - 1) as f64;
+            let g = j as f64 / (IMAGE_HEIGHT - 1) as f64;
+            let b: f64 = 0.25;
+
+            let ir = (r * 255.999) as i32;
+            let ig = (g * 255.999) as i32;
+            let ib = (b * 255.999) as i32;
+
+            let pixel = format!("{} {} {}\n", ir, ig, ib);
+            file_buffer.write(pixel.as_bytes()).unwrap();
+        }
+    }
+
+    file_buffer.flush().unwrap();
 }
